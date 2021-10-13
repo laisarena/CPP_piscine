08ef8b554666730eddeb1c8748fdb105665651b6
Lais Arena <laisarena@gmail.com>
2021-10-14 00:33:09 +0200

    Finish ex05

--- module_01/ex05/main.cpp
+++ module_01/ex05/main.cpp
@@ -6,12 +6,19 @@
 /*   By: lfrasson <laisarena@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/13 23:02:15 by lfrasson          #+#    #+#             */
-/*   Updated: 2021/10/13 23:02:39 by lfrasson         ###   ########.fr       */
+/*   Updated: 2021/10/14 00:23:36 by lfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Karen.hpp"
 
 int		main( void ) {
+	Karen	karen = Karen();
+
+	karen.complain("DEBUG");
+	karen.complain("INFO");
+	karen.complain("WARNING");
+	karen.complain("ERROR");
+	karen.complain("x");
 	return 0;
 }
