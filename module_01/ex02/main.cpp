f3e73c53b90a0f82e248835d3679ee458f56e0a5
Lais Arena <laisarena@gmail.com>
2021-10-31 00:50:36 +0200

    Correct ex02

--- module_01/ex02/main.cpp
+++ module_01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lfrasson <laisarena@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 19:10:47 by lfrasson          #+#    #+#             */
-/*   Updated: 2021/10/31 00:34:06 by coder            ###   ########.fr       */
+/*   Updated: 2021/10/31 00:49:58 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,12 @@ int		main(void)
 	std::string	*stringPTR = &string;
 	std::string	&stringREF = string;
 
+	std::cout << "ADDRESSES:" << std::endl;
 	std::cout << &string << std::endl;
 	std::cout << stringPTR << std::endl;
 	std::cout << &stringREF << std::endl;
+	std::cout << "STRING:" << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
 	return 0;
 }
