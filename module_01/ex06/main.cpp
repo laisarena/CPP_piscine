c1d5e40023602e9ffe4d4630aae7a9e6940ecbb6
Lais Arena <laisarena@gmail.com>
2021-10-31 17:20:32 +0100

    Refactor ex06

--- module_01/ex06/main.cpp
+++ module_01/ex06/main.cpp
@@ -6,13 +6,14 @@
 /*   By: lfrasson <laisarena@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/13 23:02:15 by lfrasson          #+#    #+#             */
-/*   Updated: 2021/10/16 02:15:48 by lfrasson         ###   ########.fr       */
+/*   Updated: 2021/10/31 17:20:01 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Karen.hpp"
 
-static int	getLevelIndex(std::string input, std::string *level) {
+static int	getLevelIndex(std::string input, std::string *level)
+{
 	int	i;
 
 	i = 0;
@@ -21,36 +22,38 @@ static int	getLevelIndex(std::string input, std::string *level) {
 	return i;
 }
 
-static void	applyFilter(int i, std::string *level) {
-	Karen	karen = Karen();
-
-	switch( i ) {
-		case 0 :
-			printKarenComplain(karen, level[i++]);
-		case 1 :
-			printKarenComplain(karen, level[i++]);
-		case 2 :
-			printKarenComplain(karen, level[i++]);
-		case 3 :
-			printKarenComplain(karen, level[i++]);
-			break;
-		default :
-			std::cout << "[ Probably complaining about insignificant problems ]"
-				<< std::endl;
-	}
-}
-
 static void	printKarenComplain(Karen &karen, std::string level) {
 	std::cout << "[ " << level << " ]" << std::endl;
 	karen.complain(level);
 	std::cout  << std::endl;
 }
 
-int	main( int argc, char **argv) {	
+static void	applyFilter(int i, std::string *level)
+{
+	Karen	karen = Karen();
+
+	switch(i) {
+		case 0:
+			printKarenComplain(karen, level[i++]);
+		case 1:
+			printKarenComplain(karen, level[i++]);
+		case 2:
+			printKarenComplain(karen, level[i++]);
+		case 3:
+			printKarenComplain(karen, level[i++]);
+			break;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]"
+				<< std::endl;
+	}
+}
+
+int	main(int argc, char **argv)
+{
 	int			i;
 	std::string	level[4] = {"DEBUG", "INFO" ,"WARNING", "ERROR"};
 	
-	if( argc != 2 )
+	if(argc != 2)
 		return -1;
 	i = getLevelIndex(argv[1], level);
 	applyFilter(i, level);
