bbe06aaf970a4454c8ace5e27c940eafd5fda24a
Lais Arena <laisarena@gmail.com>
2022-01-19 14:59:21 -0300

    Improve main

--- module_05/ex02/main.cpp
+++ module_05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lfrasson <lfrasson@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/20 22:31:39 by lfrasson          #+#    #+#             */
-/*   Updated: 2022/01/19 14:47:19 by lfrasson         ###   ########.fr       */
+/*   Updated: 2022/01/19 14:58:18 by lfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,74 +15,45 @@
 #include "RobotomyRequestForm.hpp"
 #include "PresidentialPardonForm.hpp"
  
-void tryExecute1(void)
+void tryExecuteNotSigned(void)
 {
-	try {
-		Bureaucrat maria = Bureaucrat("Maria", 10);
-		ShrubberyCreationForm form1 = ShrubberyCreationForm("home");
-		maria.executeForm(form1);
-	} 
-	catch (const Form::GradeTooHighException& e) {
-		std::cout << e.what() << std::endl;
-	}
-	catch (const Form::GradeTooLowException& e) {
-		std::cout << e.what() << std::endl;
-	}
+	Bureaucrat maria = Bureaucrat("Maria", 10);
+	ShrubberyCreationForm form1 = ShrubberyCreationForm("home");
+	maria.executeForm(form1);
 }
 
-void tryExecute2(void)
+void tryExecuteWrongGrade(void)
 {
-	try {
-		Bureaucrat maria = Bureaucrat("Maria", 10);
-		Bureaucrat jose = Bureaucrat("Jose", 150);
-		ShrubberyCreationForm form1 = ShrubberyCreationForm("home");
-		maria.signForm(form1);
-		jose.executeForm(form1);
-	} 
-	catch (const Form::GradeTooHighException& e) {
-		std::cout << e.what() << std::endl;
-	}
-	catch (const Form::GradeTooLowException& e) {
-		std::cout << e.what() << std::endl;
-	}
+	Bureaucrat maria = Bureaucrat("Maria", 10);
+	Bureaucrat jose = Bureaucrat("Jose", 150);
+	ShrubberyCreationForm form1 = ShrubberyCreationForm("home");
+	maria.signForm(form1);
+	jose.executeForm(form1);
 }
 
-void executeForm(void)
+void executeForm(Form &form)
 {
-	try {
-		Bureaucrat maria = Bureaucrat("Maria", 10);
-		Bureaucrat jose = Bureaucrat("Jose", 5);
-		ShrubberyCreationForm form1 = ShrubberyCreationForm("home");
-		maria.signForm(form1);
-		jose.executeForm(form1);
-	
-		RobotomyRequestForm form2 = RobotomyRequestForm ("Jorge");
-		maria.signForm(form2);
-		jose.executeForm(form2);
-		
-		PresidentialPardonForm form3 = PresidentialPardonForm("Jorge");
-		maria.signForm(form3);
-		jose.executeForm(form3);
-
-	} 
-	catch (const Form::GradeTooHighException& e) {
-		std::cout << e.what() << std::endl;
-	}
-	catch (const Form::GradeTooLowException& e) {
-		std::cout << e.what() << std::endl;
-	}
+	std::cout << std::endl << "****** Execute form *******" << std::endl;
+	Bureaucrat maria = Bureaucrat("Maria", 10);
+	Bureaucrat jose = Bureaucrat("Jose", 5);
+	maria.signForm(form);
+	jose.executeForm(form);
 }
 
 int	main(void)
 {
 	std::cout << std::endl << "****** Try execute a form that is not signed *******" << std::endl;
-	tryExecute1();
+	tryExecuteNotSigned();
 
 	std::cout << std::endl << "****** Try execute a form with wrong grade *******" << std::endl;
-	tryExecute2();
+	tryExecuteWrongGrade();
 	
-	std::cout << std::endl << "****** Execute form *******" << std::endl;
-	executeForm();
+	ShrubberyCreationForm form1 = ShrubberyCreationForm("home");
+	executeForm(form1);
+	RobotomyRequestForm form2 = RobotomyRequestForm ("Jorge");
+	executeForm(form2);
+	PresidentialPardonForm form3 = PresidentialPardonForm("Jorge");
+	executeForm(form3);
 	
 	return 0;
 }
