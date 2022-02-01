7823dccdbff91029781c993570317b210d7cbfbc
Lais Arena <laisarena@gmail.com>
2022-02-01 17:11:35 -0300

    Implement funtion detect a char

--- module_06/ex00/main.cpp
+++ module_06/ex00/main.cpp
@@ -1,5 +1,12 @@
 #include <exception>
 #include <iostream>
+#include <string>
+#include <cctype>
+
+#define CHAR	0
+#define INT		1
+#define FLOAT	2
+#define DOUBLE	3
 
 class	InputException:	public std::exception
 {
@@ -11,18 +18,42 @@ class	InputException:	public std::exception
 
 static void	checkNumberOfParameters(int argc)
 {
-	try {
-		if (argc != 2)
-			throw InputException();
-	}
-	catch (std::exception& e){
-		std::cout << e.what() << std::endl; 
-	}
+	if (argc != 2)
+		throw InputException();
+}
+
+
+static bool	isChar(std::string input)
+{
+	if (input.size() != 1)
+		return false;
+
+	char c = *input.c_str();
+
+	if (!isgraph(c))
+		return false;
+	if (isdigit(c))
+		return false;
+	return true;
+}
+
+static int	parseParameter(char* input)
+{
+	if (isChar(input))
+		return CHAR;
+	throw std::exception();
 }
 
 int	main(int argc, char** argv)
 {
-	checkNumberOfParameters(argc);
-	(void)argv;
+	try {
+		checkNumberOfParameters(argc);
+		int type = parseParameter(argv[1]);
+		type++;
+	}
+	catch (std::exception& e){
+		std::cout << e.what() << std::endl; 
+		return -1;
+	}
 	return 0;
 }
